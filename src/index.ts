7cefd1695c29eb63996af2b3947228a3e80d816c
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-10-04 09:18:17 +0300

    setup DB & userSchema

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-
+import mongoos from 'mongoose';
 
 const app = express();
 app.use(cors({
@@ -21,5 +21,10 @@ server.listen(8000, () => {
     console.log('Server running on http://localhost:8000');
 })
 
+const MONOGO_URL = 'mongodb+srv://abood75:78PUv4f0zfJYmtSl@cluster0.1it7o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoos.Promise = Promise;
+mongoos.connect(MONOGO_URL);
+mongoos.connection.on('error', (error: Error) => console.log(error))
 
 
